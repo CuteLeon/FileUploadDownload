8e05a32100e3e3e7b2b55e767c27995138d6ce95
Leon <leon.id@qq.com>
2019-07-25 11:39:05 +0800

    fixed;

--- FileUploadDownload/Utils/Helper.cs
+++ FileUploadDownload/Utils/Helper.cs
@@ -7,13 +7,14 @@
         /// </summary>
         /// <param name="length"></param>
         /// <returns></returns>
-        public static string FormatFileSize(long length) => length switch
-        {
-            _ when length < 1024 => $"{length} B",
-            _ when length < 1048576 => $"{length >> 10} KB+",
-            _ when length < 1073741824 => $"{length >> 20} MB+",
-            _ when length < 1099511627776 => $"{length >> 30} GB+",
-            _ => $"{length >> 40} TB+",
-        };
+        public static string FormatFileSize(long length) 
+            => length switch
+                {
+                    _ when length < 1024 => $"{length} B",
+                    _ when length < 1048576 => $"{length >> 10} KB+",
+                    _ when length < 1073741824 => $"{length >> 20} MB+",
+                    _ when length < 1099511627776 => $"{length >> 30} GB+",
+                    _ => $"{length >> 40} TB+",
+                };
     }
 }
